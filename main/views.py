47afae8a5299ecead611f977fe2b4b07ef493a47
ppp5649 <ppp5649@naver.com>
2022-10-05 18:01:45 +0900

    feat : realtime 추가 / dict join 코드 추가

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 import requests
+import websockets
+import asyncio
+import json
 
 # 매매기준율 (달러 환율) - 두나무 api (제공사:하나은행)
 def usd_to_krw(url):
@@ -33,17 +36,18 @@ def get_upbit_price(url):
     response_json = response.json()
     
     # key값으로 조회도 가능하고, 템플릿 변수로 사용하기 편한 dictionary로 통일
-    upbit_price = {} # 업비트 현재가 (전일 종가는 초기화 09시, UTC 기준 00시)
-    upbit_changed_rate = {} # 업비트 변동률(기준 안나와있음) -> 수정 필요 빗썸과 기준 다름
-    upbit_trade_volume = {} # 업비트 최근 24시간 거래량(원화)
+    upbit_price = {} 
+    upbit_changed_rate = {} 
+    upbit_trade_volume = {} 
     
     # for문 안에 변수들은 함수 내에서만 쓰이는 변수이기 때문에 변수명 크게 신경 X
     for i in range(0, len(response_json)):
-        trade_price = response_json[i]['trade_price']
-        prev_closing_price = response_json[i]['prev_closing_price']
-        changed_rate = ((trade_price-prev_closing_price)/prev_closing_price*100)
-        ticker = response_json[i]['market']
-        acc_trade_price_24h = response_json[i]['acc_trade_price_24h']
+        
+        trade_price = response_json[i]['trade_price'] # 현재가 
+        prev_closing_price = response_json[i]['prev_closing_price'] # 전일종가(초기화 09시)
+        changed_rate = ((trade_price-prev_closing_price)/prev_closing_price*100) # 변동률(직접계산)
+        ticker = response_json[i]['market'] # 티커
+        acc_trade_price_24h = response_json[i]['acc_trade_price_24h'] # 최근 24시간 거래량(원화)
 
         upbit_price.update({ticker : format(trade_price, ',')}) 
         # 현재가 3자리마다 , 추가 (단 자료형이 숫자형일 경우만 가능 str X)
@@ -51,7 +55,7 @@ def get_upbit_price(url):
         upbit_trade_volume.update({ticker : round(acc_trade_price_24h/100000000)})
         # float -> round로 깎고 억 단위로 나누기
 
-    return upbit_price, upbit_changed_rate, upbit_trade_volume # return 값 여러개 가져감
+    return upbit_price, upbit_changed_rate, upbit_trade_volume # return 값 다수
 
 
 def get_bithumb_price(url): 
@@ -129,9 +133,8 @@ def get_korbit_price(url):
 
 def index(request):
     usd_price = usd_to_krw("https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD")
-    upbit_ticker_dict = get_upbit_ticker("https://api.upbit.com/v1/market/all") 
-    upbit_ticker_list = list(upbit_ticker_dict.keys()) # type을 dict_keys -> list로 변환
-    upbit_ticker_str = ','.join(upbit_ticker_list) # url에 넣어주기 위해 list -> str 변환
+    upbit_ticker_dict = get_upbit_ticker("https://api.upbit.com/v1/market/all")     
+    upbit_ticker_str = ",".join(upbit_ticker_dict) # url에 넣어주기 위해 list -> str 변환
     
     # 4대 거래소 시세 정보
     upbit_price, upbit_changed_rate, upbit_trade_volume = get_upbit_price(f"https://api.upbit.com/v1/ticker?markets={upbit_ticker_str}") 
@@ -142,7 +145,13 @@ def index(request):
 
     korbit_price, korbit_changed_rate, korbit_trade_volume = get_korbit_price("https://api.korbit.co.kr/v1/ticker/detailed/all")
 
+    # print(korbit_price)
+
     # 템플릿 변수
     context = {'usd_price' : usd_price, 'upbit_ticker_dict':upbit_ticker_dict, 'upbit_price':upbit_price, 'upbit_changed_rate':upbit_changed_rate, 'upbit_trade_volume':upbit_trade_volume, 'bithumb_price':bithumb_price, 'bithumb_changed_rate':bithumb_changed_rate, 'bithumb_trade_volume':bithumb_trade_volume,'coinone_price':coinone_price, 'coinone_changed_rate':coinone_changed_rate, 'coinone_trade_volume':coinone_trade_volume, 'korbit_price':korbit_price, 'korbit_changed_rate':korbit_changed_rate, 'korbit_trade_volume':korbit_trade_volume}
 
     return render(request, 'index.html', context)
+
+def realtime(request):
+    context = {'abc' : 'hello'}
+    return render(request, "realtime.html", context)
